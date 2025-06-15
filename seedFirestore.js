dbb2e96f780fa40ac6b429645a0b23ebc4e4747d
Ephius Mutambo <114339494+EphiusJB@users.noreply.github.com>
2025-06-15 18:49:40 +0200

    Update seedFirestore.js

--- seedFirestore.js
+++ seedFirestore.js
@@ -3,15 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, setDoc, doc, collection } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA9lFGa3Ry3l9SotYxR_awFGfIJBgZv_jM",
-  authDomain: "ol-mall-77c64.firebaseapp.com",
-  projectId: "ol-mall-77c64",
-  storageBucket: "ol-mall-77c64.firebasestorage.app",
-  messagingSenderId: "120293824160",
-  appId: "1:120293824160:web:0077c0fc77510641ce1bc5",
-  measurementId: "G-330Z7D6QF8"
-};
+const firebaseConfig = {}
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
